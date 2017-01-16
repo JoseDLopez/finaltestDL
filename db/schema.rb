ff807f37e62ec60aeb3232c2295da8198372f0d6
JoseDLopez <josed-lopez@hotmail.com>
2017-01-16 19:55:01 -0300

    Hacemos la relacion entre los usuarios y las empresas, ademas agregamos las columnas de edad, nombre y empresa al devise. Nos falta hacer que sea un parametro permitido

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116224654) do
+ActiveRecord::Schema.define(version: 20170116225124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,14 @@ ActiveRecord::Schema.define(version: 20170116224654) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.integer  "age"
+    t.integer  "company_id"
+    t.index ["company_id"], name: "index_users_on_company_id", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "claims", "users"
+  add_foreign_key "users", "companies"
 end
