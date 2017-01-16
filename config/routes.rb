4aa38be52d36dd3734219137596b73ab64d333f6
JoseDLopez <josed-lopez@hotmail.com>
2017-01-16 20:36:50 -0300

    Se redirige al usuario loggeado al index de las empresas, y se bloquea el acceso a las demas zonas del programa, falta que cuando un usuario cree una compañia esta compañia sea asociada directamente a el

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :claims
-  resources :companies
+  
+  resources :companies do
+  	resources :claims, only: [:show, :create, :new]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "companies#index"
 
 	devise_for :users, controllers: {
-		registrations: 'users/registrations'
+		registrations: 'users/registrations',
+		sessions: 'users/sessions'
 	}
 
 end
