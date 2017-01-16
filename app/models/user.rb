31a014a508a8e111d8535ddacbe88c119f8986d7
JoseDLopez <josed-lopez@hotmail.com>
2017-01-16 20:23:46 -0300

    Se logra registrar el usuario sin colocar la empresa de una vez

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # before_create :set_default_company
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  belongs_to :companies
+  belongs_to :companies, optional: true
   has_many :claims
+
+  # def set_default_company
+  #   self.company ||= 0000
+  # end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :claims
-  devise_for :users
   resources :companies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "companies#index"
+
+	devise_for :users, controllers: {
+		registrations: 'users/registrations'
+	}
+
 end
