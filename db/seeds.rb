c128ad31a7e99b1e2f23cfaa3584f5d32bb78453
JoseDLopez <josed-lopez@hotmail.com>
2017-01-16 22:54:30 -0300

    Se modifica el seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,52 +11,47 @@ Claims.destroy_all
 User.destroy_all
 Company.destroy_all
 
-user_list = []
+# user_list = []
+# 30.times.do
+#   name = Faker::Name.first_name
+#   age = Faker::Number.between(20, 80)
+#   email = Faker::Internet.email
+#   password = Faker::Internet.password(6)
+#   password_confirmation = password
+#   # Add user to user_list
+#   user_list << [ name, age, email, password, password_confirmation]
+# end
 
-30.times.do
-  name = Faker::Name.first_name
-  age = Faker::Number.between(20, 80)
-  email = Faker::Internet.email
-  password = Faker::Internet.password(6)
-  password_confirmation = password
-
-  # Add user to user_list
-  user_list << [ name, age, email, password, password_confirmation]
-end
-
-user_list.each do |name, age, email, password, password_confirmation|
-  User.create(name: name, age: age, email: email,  password: password, password_confirmation: password_confirmation)
-end
+# user_list.each do |name, age, email, password, password_confirmation|
+#   User.create(name: name, age: age, email: email,  password: password, password_confirmation: password_confirmation)
+# end
 
 
 
-company_list = []
+# company_list = []
 
-20.times.do
-  name = Faker::Name.title
+# 20.times.do
+#   name = Faker::Name.title
+#   # Add user to user_list
+#   company_list << [name]
+# end
 
-
-  # Add user to user_list
-  company_list << [name]
-end
-
-company_list.each do |name|
-  User.create(name: name)
-end
+# company_list.each do |name|
+#   Company.create(name: name)
+# end
 
 
-claims_list = []
+# claims_list = []
 
-250.times.do
-  title = Faker::Name.title
-  content = 
-  user_id = Faker::Number.between(16, 36)
+# 250.times.do
+#   title = Faker::Name.title
+#   content = Faker::Lorem.sentence(3)
+#   user_id = Faker::Number.between(16, 36)
+#   created_at = Faker::Date.between(30.days.ago, Date.today) #=> "Wed, 24 Sep 2014"
+#   # Add user to user_list
+#   claims_list << [title,content,user_id,created_at]
+# end
 
-
-  # Add user to user_list
-  company_list << [name]
-end
-
-company_list.each do |name|
-  User.create(name: name)
-end
+# claims_list.each do |title,content,user_id,created_at|
+#   Claim.create(title: title, content: content, user_id: user_id, created_at: created_at)
+# end
